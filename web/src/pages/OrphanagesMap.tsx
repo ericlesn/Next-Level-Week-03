c36a1ddd3da7543ad2dd2c0a6174a649cca42b7a
ericlesn <ericlesnunes1@outlook.com>
2020-10-22 13:19:38 -0300

    bug do mapa arrumado

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import {FiPlus} from 'react-icons/fi'
-import {Link} from 'react-router-dom'
-import {Map,TileLayer} from 'react-leaflet'
-import '../styles/pages/orphanages-map.css'
+import {FiPlus} from 'react-icons/fi';
+import {Link} from 'react-router-dom';
+import {Map,TileLayer, Marker, Popup} from 'react-leaflet';
+import '../styles/pages/orphanages-map.css';
+import Leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import mapMarkerImg from '../images/map-marker.svg';
 
-import mapMarkerImg from '../images/map-marker.svg'
-import {  } from 'leaflet';
+
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+
+    iconSize:[58,68],
+    iconAnchor:[29,68]
+})
 
 function OrphanagesMap(){
     return(
@@ -23,10 +32,20 @@ function OrphanagesMap(){
                 </footer>
             </aside>
             <Map
-                center={[-27.123,-49.231]}
+                center={[-23.8258131,-46.1412139]}
                 zoom={15}
                 style={{width:'100%', height:'100%'}}>
                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            
+            
+                <Marker
+                    icon={mapIcon}
+                    position={[-27.209252,-49.6401092]}
+
+                    /*<Popup closeButton={false}>
+                        </Popup>*/
+
+                />
             </Map>
 
             <Link to="" className="create-orphanage">
